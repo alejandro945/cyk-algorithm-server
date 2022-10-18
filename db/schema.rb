7eea6d566ee9470bc9904d9cbdea45fade619187
alejandroTTT <alejandro@twototango.co>
2022-10-17 22:47:30 -0500

    Db mogration:whale:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_17_025607) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_18_031215) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "responses", force: :cascade do |t|
     t.string "word"
     t.boolean "isAdmitted"
